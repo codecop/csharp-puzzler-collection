cd7bd6454cbe4c6dcecf06b71068a14498030cc2
Peter Kofler <peter.kofler@code-cop.org>
2019-12-18 23:01:50 +0100

    Format C# 8 code.

--- Puzzler/CSharpEight.cs
+++ Puzzler/CSharpEight.cs
@@ -1,39 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Puzzler
 {
-    class CSharpEight
+    public class CSharpEight
     {
         public void Run()
         {
 
         }
 
-
     }
 
-    public struct Point
+    public struct PointReadonlyProperty
     {
         public double X { get; set; }
         public double Y { get; set; }
-        public double Distance => Math.Sqrt(X * X + Y * Y);
 
-        //public readonly override string ToString() =>
-        //    $"({X}, {Y}) is {Distance} from the origin";
+        // needs readonly modifier because of ToString
+        public readonly double Distance => Math.Sqrt(X * X + Y * Y);
+
+        public readonly override string ToString() =>
+           $"({X}, {Y}) is {Distance} from the origin";
     }
+
     public interface ICustomer
     {
         DateTime DateJoined { get; }
         DateTime? LastOrder { get; }
         string Name { get; }
         IDictionary<DateTime, string> Reminders { get; }
+
+        // Default method in interfaces
         public string HelloCustomer()
         {
             return "hi";
         }
     }
+
     public class Customer : ICustomer
     {
         public DateTime DateJoined => throw new NotImplementedException();
